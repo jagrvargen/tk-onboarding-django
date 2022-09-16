4b2d68bf9024f31a91e746be51dd91fc63bfeb3c
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-16 11:22:16 +0200

    added recipe model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,14 @@
+"""
+Model definitions.
+"""
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    """Model definition for a recipe."""
+
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=1024, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
---
+++ app/recipes/tests/test_recipes_api.py
@@ -0,0 +1,13 @@
+"""
+Contains tests for the recipes API.
+"""
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+
+class RecipeAPITests(TestCase):
+    """Tests the recipes API."""
+    pass
