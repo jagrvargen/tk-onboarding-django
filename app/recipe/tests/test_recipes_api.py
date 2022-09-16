639e0c677e419087bfdec2350edfc7331c6dd195
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-16 15:30:26 +0200

    added tests and urlpatterns for recipe model

---
+++ app/recipe/tests/test_recipes_api.py
@@ -0,0 +1,32 @@
+"""
+Contains tests for the recipe API.
+"""
+from django.urls import reverse
+from django.test import TestCase
+
+from rest_framework import status
+from rest_framework.test import APIClient
+
+from core.models import Recipe
+from recipe.serializers import RecipeSerializer
+
+RECIPES_URL = reverse('recipe:recipe-list')
+
+
+class RecipeAPITests(TestCase):
+    """Tests the recipe API."""
+
+    def setUp(self):
+        self.client = APIClient()
+
+    def retrieve_recipes(self):
+        """Test retrieve all recipes from REST API."""
+        Recipe.objects.create(name='Salad')
+        Recipe.objects.create(name='Sauce')
+
+        res = self.client.get(RECIPES_URL)
+        recipes = Recipe.objects.all().order_by('-id')
+        serializer = RecipeSerializer(recipes, many=True)
+
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        self.assertEqual(res.data, serializer.data)
