bc4629a0e443cc3dbb45c02168346c170aa6dd95
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-16 15:27:11 +0200

    added drf_spectacular to installed apps

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'drf_spectacular',
     'core',
-    'recipes',
+    'recipe',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
