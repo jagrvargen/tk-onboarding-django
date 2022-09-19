f233df935a58c935ea5f9f635a5f3e68b4ab9fe6
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-19 14:56:32 +0200

    add ingredient model

--- app/core/models.py
+++ app/core/models.py
@@ -9,6 +9,16 @@ class Recipe(models.Model):
 
     name = models.CharField(max_length=255)
     description = models.CharField(max_length=1024, null=True, blank=True)
+    ingredients = models.ManyToManyField('Ingredient')
+
+    def __str__(self):
+        return self.name
+
+
+class Ingredient(models.Model):
+    """Model definition for an ingredient"""
+
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
