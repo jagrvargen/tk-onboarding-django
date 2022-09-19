eadd4ef4f097f3980acb634de732cc0f0b803efa
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-19 15:00:00 +0200

    added ingredient view set

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,10 +1,10 @@
 """
 Recipe viewsets
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 
-from core.models import Recipe
-from recipe.serializers import RecipeSerializer
+from core.models import Recipe, Ingredient
+from recipe.serializers import RecipeSerializer, IngredientSerializer
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
@@ -13,9 +13,30 @@ class RecipeViewSet(viewsets.ModelViewSet):
     queryset = Recipe.objects.all()
 
     def get_queryset(self):
-        """Fetch all recipes."""
+        """Fetch recipes."""
         name = self.request.query_params.get('name')
         if name:
             return self.queryset.filter(name=name)
 
         return self.queryset.order_by('-id')
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save()
+
+
+class IngredientViewSet(mixins.DestroyModelMixin,
+                        mixins.UpdateModelMixin,
+                        mixins.ListModelMixin,
+                        viewsets.GenericViewSet):
+    """Viewset for the ingredient API."""
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
+
+    def get_queryset(self):
+        """Fetch ingredients."""
+        return self.queryset.order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new recipe."""
+        serializer.save()
