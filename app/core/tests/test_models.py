dfb0f014f1a26a501bda1055ab04dfeb13589179
jagrvargen <24496783+jagrvargen@users.noreply.github.com>
2022-09-19 14:56:56 +0200

    added test for creating an ingredient

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,16 +3,27 @@ Tests for custom models.
 """
 from django.test import TestCase
 
-from core.models import Recipe
+from core.models import Recipe, Ingredient
 
 
-class RecipeTests(TestCase):
+class ModelTests(TestCase):
     """Tests for the recipe model."""
 
-    def test_create_recipe_successful(self):
-        """Test a recipe is successfully created."""
+    def test_create_recipe(self):
+        """Test creating a recipe."""
         name = 'Foie Gras'
         recipe = Recipe.objects.create(name=name)
 
         self.assertEqual(recipe.name, name)
         self.assertEqual(str(recipe), name)
+
+    def test_create_ingredient(self):
+        """Test creating an ingredient."""
+        recipe_name = 'Foie Gras'
+        recipe = Recipe.objects.create(name=recipe_name)
+
+        ingredient_name = 'Salt'
+        ingredient = Ingredient.objects.create(name=ingredient_name, recipe=recipe)
+
+        self.assertEqual(ingredient.name, ingredient_name)
+        self.assertEqual(str(ingredient), ingredient_name)
